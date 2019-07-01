0551f6f3e045c10f955f674281fda8605b97944a
Paul Spiesberger <paul.spiesberger@rise-world.com>
2019-07-01 11:19:37 +0200

    adds empty first Android project

---
+++ app/src/main/java/at/ict4d/weather2/MainActivity.kt
@@ -0,0 +1,12 @@
+package at.ict4d.weather2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
